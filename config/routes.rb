7f2874dbeca4f46d1181d6714e1812250a2ef036
psharma <psharma8rrc.ca>
2024-06-27 15:03:56 -0500

    Added some new stuffs

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,31 @@
+# Rails.application.routes.draw do
+#   # devise_for :users
+#   # ActiveAdmin.routes(self)
+
+#   resources :courses do
+#     resources :lessons do
+#       resources :discussions
+#     end
+#   end
+
+#   resources :enrollments
+#   resources :users
+
+#   # get 'about', to: 'static_pages#about'
+#   
+# end
+
+
 Rails.application.routes.draw do
-  # devise_for :users
-  # ActiveAdmin.routes(self)
-
   resources :courses do
-    resources :lessons do
-      resources :discussions
-    end
+    resources :lessons, only: [:index, :new, :create]
   end
-
+  resources :lessons, except: [:index, :new, :create] do
+    resources :discussions, only: [:index, :new, :create]
+  end
+  resources :discussions, except: [:index, :new, :create]
   resources :enrollments
   resources :users
 
-  # get 'about', to: 'static_pages#about'
   root to: 'courses#index'
 end
