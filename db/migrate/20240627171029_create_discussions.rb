e0214ce63db04b21ac2f5a60664256677c54d083
psharma <psharma8rrc.ca>
2024-06-27 12:10:42 -0500

    added discussion model

---
+++ db/migrate/20240627171029_create_discussions.rb
@@ -0,0 +1,11 @@
+class CreateDiscussions < ActiveRecord::Migration[7.1]
+  def change
+    create_table :discussions do |t|
+      t.text :content
+      t.references :user, null: false, foreign_key: true
+      t.references :lesson, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
