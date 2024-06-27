3842da3d0541b413b3900fe0cf0bf362ab49b0b6
psharma <psharma8rrc.ca>
2024-06-27 12:17:58 -0500

    added courses and enrollments model contente

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,12 @@
 class Course < ApplicationRecord
-end
+    has_many :enrollments
+    has_many :users, through: :enrollments
+    has_many :lessons
+  
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  
+    # has_one_attached :image
+  end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-    has_secure_passwordhas
+    has_secure_password
     has_many :enrollments
-    has_many :courses, through :enrollments
+    has_many :courses, through: :enrollments
     has_many :discussions
-
-    validates :name, presence: true,
+  
+    validates :name, presence: true
     validates :email, presence: true, uniqueness: true
-end
+  end
+  
