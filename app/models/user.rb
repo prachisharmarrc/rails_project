94757bf154ee545a7155d5ac51b057ecbe849df5
psharma <psharma8rrc.ca>
2024-06-27 12:13:50 -0500

    Added user model code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_passwordhas
+    has_many :enrollments
+    has_many :courses, through :enrollments
+    has_many :discussions
+
+    validates :name, presence: true,
+    validates :email, presence: true, uniqueness: true
 end
